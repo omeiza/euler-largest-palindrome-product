5becd52b0e7faa378492f2d5b0c9526ade0009ae
Omeiza Owuda <owudaomeiza@gmail.com>
2021-08-31 02:14:05 +0100

    Project euler prime factor 600851475143

---
+++ solutions/prime-factor.js
@@ -0,0 +1,4 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+// Define prime factors: Well, they are factors of a number that are prime. i.e prime are numbers that are divisible by 1 and itself
