b63e75f7564ea8b97bae6845eae41c0fcc1a22f8
Omeiza Owuda <owudaomeiza@gmail.com>
2023-05-03 22:59:32 +0100

    2 problems converted to typescript code

---
+++ problem-1.ts
@@ -0,0 +1,21 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9.
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000
+ * Reference: https://projecteuler.net/problem=1
+ */
+
+// For general purpose, it is useful to create a function that takes a parameter as limit of multiples
+function sumOfMultiples(limit: number): number {
+	let total = 0;
+
+	for (let x = 1; x < limit; x++) {
+		if ((x % 3 === 0) || (x % 5 === 0)) {
+			total = total + x;
+		}
+	}
+
+	return total;
+}
+
+// Call function with limit 100
+console.log(sumOfMultiples(1000));
